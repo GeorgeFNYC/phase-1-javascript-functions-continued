695f8b98297c4634145048e1315d37f2b6df955c
George Fernandez <fernandezg704@gmail.com>
2022-08-31 14:31:38 -0400

    commit tree 5b2c7a8466be889debc6b8634f4e2e7a1aa91243

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun (param = 'roller-skate') {
+    return `This Saturday, I want to ${param}!`
+};
+
+function mondayWork (objective = 'go to the office') {
+    return `This Monday, I will ${objective}.`;
+};
+// function wrapAdjective (adjTwo = '*') {
+//     (function (adj = 'special') {
+//         const prompt = wrapAdjective("%")("a dedicated programmer");
+//         return prompt
+//     });
+//     return `You are ${adjTwo}!`
+// }
+
+
+function wrapAdjective(start = "*") {
+    return function (word = "special") {
+      return `You are ${start}${word}${start}!`;
+    };
+  }
+  
+ 
